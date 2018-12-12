e44c057f7d00423d1c0a2f7e1e69ec4a3abbd3b7
Timpupk <39681379+Timpupk@users.noreply.github.com>
2018-12-12 11:37:09 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var v = [];
+var font;
+var points = [];
+
+function preload() {
+  font = loadFont('font.TTF');
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  points = font.textToPoints('Sparticles', 50, 200, 60, {
+    sampleFactor: 1
+  });
+
+  for (var i = 0; i < points.length; i ++){
+    v.push({x:random(0, width), y:random(0, height), sz:2, m:5, a:0, tx:points[i].x, ty:points[i].y});
+  }
+}
+function draw() {
+  background(250);
+
+  for (var i = 0; i < v.length; i ++){
+    var ve = v[i];
+
+    strokeWeight(ve.sz);
+    stroke(200, 0, 0);
+    point(ve.x, ve.y);
+
+    ve.x-=ve.m*sin(ve.a);
+    ve.y-=ve.m*cos(ve.a);
+
+    ve.a = -atan2(ve.ty-ve.y, ve.tx-ve.x)-90;
+
+    if (ve.x == ve.tx && ve.y == ve.ty){ve.m = 0;}else{
+      ve.m = map(dist(ve.x, ve.y, ve.tx, ve.ty), 0, width/2+height/2, 0.01, 10);
+    }
+  }
+}
