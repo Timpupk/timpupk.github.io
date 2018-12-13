bb8fb4ed536b799615e55a16eec7fafaf4e068f5
Timpupk <39681379+Timpupk@users.noreply.github.com>
2018-12-12 17:55:07 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ function preload() {
 function setup() {
   createCanvas(window.innerWidth, window.innerWidth);
 
-  points = font.textToPoints('EXAMPLE TEXT', width/16+height/16, 200, width/16+height/16, {
+  points = font.textToPoints('TEXT', width/16+height/16, 200, width/16+height/16, {
     sampleFactor: 1
   });
 
